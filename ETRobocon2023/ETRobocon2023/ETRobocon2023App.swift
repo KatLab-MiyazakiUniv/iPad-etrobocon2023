de5a1b047ea2230e8c8f24b6b81f27142d3dca9e
OnagaHaruki <haloupgarou@gmail.com>
2023-07-01 21:26:58 +0900

    create project

---
+++ ETRobocon2023/ETRobocon2023/ETRobocon2023App.swift
@@ -0,0 +1,17 @@
+//
+//  ETRobocon2023App.swift
+//  ETRobocon2023
+//
+//  Created by オナガ・ハルキ on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct ETRobocon2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
