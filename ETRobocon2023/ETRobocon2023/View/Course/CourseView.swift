6407544516a27404a1dcdba9736e984a575149d9
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 01:15:44 +0900

    add: CourseView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+/// コースマップ全体を表すビュー
+struct CourseView: View {
+    var body: some View {
+        ZStack {
+
+        } // ZStack
+    } // var body
+} // struct CourseView
+
+struct CourseView_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
