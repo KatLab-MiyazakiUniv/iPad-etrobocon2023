cfb1d99062d4d6159d7260d2cbe920e753900b17
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 01:01:46 +0900

    update: コースを描画する際の始点となる座標を追加した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseSize/LCourseViewSize.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseSize/LCourseViewSize.swift
@@ -3,5 +3,15 @@ import Foundation
 /// Lコースのビューに関する数値
 /// 競技規約上のコースの寸法ではなく，アプリでビューを描画する際に必要な数値
 struct LCourseViewSize {
+    // スタート位置の青線を描画し始めるx座標
+    static var startX: CGFloat {
+        LCourseSize.blackLineCornerRadius
+        + LCourseSize.bottomBlackStraightLineLength
+        + LCourseSize.blackLineCornerRadius
+    }
 
+    // スタート位置の青線を描画し始めるy座標
+    static var startY: CGFloat {
+        0
+    }
 } // struct LCourseViewSize
