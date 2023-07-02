1a7943a25a56af455d0343f672e9d502b63a89e4
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 00:48:26 +0900

    update: ライントレースに関する寸法を追加した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseSize/LCourseSize.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseSize/LCourseSize.swift
@@ -2,5 +2,20 @@ import Foundation
 
 /// Lコースの各部分の寸法
 struct LCourseSize {
+    // MARK: ライントレース
+    // 線の太さ
+    // ライントレースの黒線や，ブロックdeトレジャーの円，青線
+    static let lineWidth: CGFloat = 20
 
+    // コース右側の，スタート直後の直進の長さ
+    static let rightBlackStraightLineLength: CGFloat = 2980
+
+    // コース下部の直進の長さ
+    static let bottomBlackStraightLineLength: CGFloat = 1815
+
+    // コース左側の直進の長さ
+    static let leftBlackStraightLineLength: CGFloat = 1840
+
+    // コース右下と左下の黒いカーブを円周の1/4としたとき，その円の半径
+    static let blackLineCornerRadius: CGFloat = 210
 } // struct LCourseSize
