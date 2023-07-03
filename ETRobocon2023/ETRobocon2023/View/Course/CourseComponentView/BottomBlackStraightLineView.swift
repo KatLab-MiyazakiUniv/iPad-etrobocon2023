d6d7f9efeefc7c51aa44838209ea534bd236e276
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 12:15:03 +0900

    add: BottomBlackStraightLineView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BottomBlackStraightLineView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+/// コース下部の，水平の直進
+struct BottomBlackStraightLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct BottomBlackStraightLineView
+
+struct BottomBlackStraightLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomBlackStraightLineView()
+    }
+}
