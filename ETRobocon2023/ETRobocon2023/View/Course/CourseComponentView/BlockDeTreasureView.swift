a95f45d64ddf342b7cdf7848622c6e281ae0b330
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 16:18:51 +0900

    add: BlockDeTreasureView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+/// ブロックdeトレジャーの4色の円と格子の組
+struct BlockDeTreasureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlockDeTreasureView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockDeTreasureView()
+    }
+}
