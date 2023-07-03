286771c0b4d0bf0ea0391bc640e3e97ab71d0380
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:28:20 +0900

    add: BlockDeTreasureFourCirclesView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureFourCirclesView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// ブロックdeトレジャーの，同じ色の4つの円の組
+struct BlockDeTreasureFourCirclesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct BlockDeTreasureFourCirclesView
+
+struct BlockDeTreasureFourCirclesView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockDeTreasureFourCirclesView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
