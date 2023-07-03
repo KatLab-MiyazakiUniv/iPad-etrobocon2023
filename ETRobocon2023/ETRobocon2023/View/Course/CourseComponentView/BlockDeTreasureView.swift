c3df517adc8d1f652f9e0004ff398db1d2a3866a
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 16:23:39 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureView.swift
@@ -2,6 +2,50 @@ import SwiftUI
 
 /// ブロックdeトレジャーの4色の円と格子の組
 struct BlockDeTreasureView: View {
+    // 左下の青い円の中心
+    private var centerXOfBottomLeftCircle: CGFloat {
+        LCourseViewSize.startX
+        - LCourseSize.blackLineCornerRadius
+        - LCourseSize.bottomBlackStraightLineLength
+        - LCourseSize.blackLineCornerRadius
+        + LCourseSize.distanceBetweenLineTraceEndAndTreasure
+    }
+
+    private var centerYOfBottomLeftCircle: CGFloat {
+        LCourseViewSize.startY
+        + LCourseSize.startBlueLineLength
+        + LCourseSize.rightBlackStraightLineLength
+        - LCourseSize.leftBlackStraightLineLength
+        - LCourseSize.lineHalfWidth
+        - LCourseSize.treasureCircleRadius
+    }
+
+    private var centerOfBottomLeftBlue: CGPoint {
+        CGPoint(x: centerXOfBottomLeftCircle, y: centerYOfBottomLeftCircle)
+    }
+
+    // 緑の円のうち，左下のものの中心
+    private var centerOfBottomLeftGreen: CGPoint {
+        CGPoint(x: centerXOfBottomLeftCircle
+                + LCourseSize.distanceBetweenCenterOfCirclesInBlockDeTreasure*2,
+                y: centerYOfBottomLeftCircle)
+    }
+
+    // 赤の円のうち，左下のものの中心
+    private var centerOfBottomLeftRed: CGPoint {
+        CGPoint(x: centerXOfBottomLeftCircle,
+                y: centerYOfBottomLeftCircle
+                - LCourseSize.distanceBetweenCenterOfCirclesInBlockDeTreasure*2)
+    }
+
+    // 黄色の円のうち，左下のものの中心
+    private var centerOfBottomLeftYellow: CGPoint {
+        CGPoint(x: centerXOfBottomLeftCircle
+                + LCourseSize.distanceBetweenCenterOfCirclesInBlockDeTreasure*2,
+                y: centerYOfBottomLeftCircle
+                - LCourseSize.distanceBetweenCenterOfCirclesInBlockDeTreasure*2)
+    }
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
