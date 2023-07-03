41787a1552774a488c20c13c6c722d21a1d06ab4
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:00:37 +0900

    add: DoubleLoopExitBlackLineView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/DoubleLoopExitBlackLineView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// ダブルループ後，コース下部の水平の直進と合流するカーブ
+struct DoubleLoopExitBlackLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct DoubleLoopExitBlackLineView
+
+struct DoubleLoopExitBlackLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        DoubleLoopExitBlackLineView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
