84ab7d94377717668020c160b49add921a6ba9cd
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 14:53:34 +0900

    add: AreaBView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/AreaBView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+/// 配置エリアB．
+/// 外側の黒い円，内側の黄色の円，中心の点線の円，青線を描画する．
+struct AreaBView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AreaBView_Previews: PreviewProvider {
+    static var previews: some View {
+        AreaBView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
