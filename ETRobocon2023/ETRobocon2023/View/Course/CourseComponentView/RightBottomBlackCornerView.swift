2b787f531bf2d9c0451aeaff954cb142d236b5e6
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 12:08:05 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/RightBottomBlackCornerView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/RightBottomBlackCornerView.swift
@@ -2,6 +2,45 @@ import SwiftUI
 
 /// コース右下の，黒線のカーブ
 struct RightBottomBlackCornerView: View {
+    // start point
+    private var startX: CGFloat {
+        LCourseViewSize.startX
+    }
+
+    private var startY: CGFloat {
+        LCourseViewSize.startY + LCourseSize.startBlueLineLength + LCourseSize.rightBlackStraightLineLength
+    }
+
+    private var startPoint: CGPoint {
+        CGPoint(x: startX, y: startY)
+    }
+
+    // end point
+    private var endX: CGFloat {
+        startX - LCourseSize.blackLineCornerRadius
+    }
+
+    private var endY: CGFloat {
+        startY + LCourseSize.blackLineCornerRadius
+    }
+
+    private var endPoint: CGPoint {
+        CGPoint(x: endX, y: endY)
+    }
+
+    // control point
+    private var controlX: CGFloat {
+        startX
+    }
+
+    private var controlY: CGFloat {
+        endY
+    }
+
+    private var controlPoint: CGPoint {
+        CGPoint(x: controlX, y: controlY)
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
