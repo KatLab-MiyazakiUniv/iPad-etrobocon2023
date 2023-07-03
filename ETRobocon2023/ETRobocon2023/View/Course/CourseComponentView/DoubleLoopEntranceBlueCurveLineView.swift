e2c250c78ae26705add9448e21282106cf216c66
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:12:51 +0900

    add: DoubleLoopEntranceBlueCurveLineView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/DoubleLoopEntranceBlueCurveLineView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// ダブルループ入りのカーブの青線
+struct DoubleLoopEntranceBlueCurveLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct DoubleLoopEntranceBlueCurveLineView
+
+struct DoubleLoopEntranceBlueCurveLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        DoubleLoopEntranceBlueCurveLineView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
