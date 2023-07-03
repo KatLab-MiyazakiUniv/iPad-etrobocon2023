c981c3c91d55bc342da502399265ff9228f61243
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 23:37:11 +0900

    update: プロパティのpathを参照するように変更した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BottomBlackStraightLineView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BottomBlackStraightLineView.swift
@@ -2,31 +2,31 @@ import SwiftUI
 
 /// コース下部の，水平の直進
 struct BottomBlackStraightLineView: View {
-    // start point
-    private var startX: CGFloat {
-        LCourseViewSize.startX - LCourseSize.blackLineCornerRadius
-    }
-
-    private var startY: CGFloat {
-        LCourseViewSize.startY + LCourseSize.startBlueLineLength + LCourseSize.rightBlackStraightLineLength + LCourseSize.blackLineCornerRadius
-    }
-
-    private var startPoint: CGPoint {
-        CGPoint(x: startX, y: startY)
-    }
-
-    // end point
-    private var endX: CGFloat {
-        startX - LCourseSize.bottomBlackStraightLineLength
-    }
-
-    private var endY: CGFloat {
-        startY
-    }
-
-    private var endPoint: CGPoint {
-        CGPoint(x: endX, y: endY)
-    }
+//    // start point
+//    private var startX: CGFloat {
+//        LCourseViewSize.startX - LCourseSize.blackLineCornerRadius
+//    }
+//
+//    private var startY: CGFloat {
+//        LCourseViewSize.startY + LCourseSize.startBlueLineLength + LCourseSize.rightBlackStraightLineLength + LCourseSize.blackLineCornerRadius
+//    }
+//
+//    private var startPoint: CGPoint {
+//        CGPoint(x: startX, y: startY)
+//    }
+//
+//    // end point
+//    private var endX: CGFloat {
+//        startX - LCourseSize.bottomBlackStraightLineLength
+//    }
+//
+//    private var endY: CGFloat {
+//        startY
+//    }
+//
+//    private var endPoint: CGPoint {
+//        CGPoint(x: endX, y: endY)
+//    }
 
     private let path: (inout Path) -> Void = { path in
         // start point
@@ -65,8 +65,7 @@ struct BottomBlackStraightLineView: View {
 
     var body: some View {
         Path { path in
-            path.move(to: startPoint)
-            path.addLine(to: endPoint)
+            self.path(&path)
         }
         .stroke(lineWidth: LCourseSize.lineWidth)
         .fill(.black)
