9bead84e168c498a4b9378c04c6958c27ca8b31d
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 16:26:23 +0900

    update: ブロックdeトレジャーの4色の円と格子の組を描画した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureView.swift
@@ -10,7 +10,7 @@ struct BlockDeTreasureView: View {
         - LCourseSize.blackLineCornerRadius
         + LCourseSize.distanceBetweenLineTraceEndAndTreasure
     }
-
+    
     private var centerYOfBottomLeftCircle: CGFloat {
         LCourseViewSize.startY
         + LCourseSize.startBlueLineLength
@@ -19,25 +19,25 @@ struct BlockDeTreasureView: View {
         - LCourseSize.lineHalfWidth
         - LCourseSize.treasureCircleRadius
     }
-
+    
     private var centerOfBottomLeftBlue: CGPoint {
         CGPoint(x: centerXOfBottomLeftCircle, y: centerYOfBottomLeftCircle)
     }
-
+    
     // 緑の円のうち，左下のものの中心
     private var centerOfBottomLeftGreen: CGPoint {
         CGPoint(x: centerXOfBottomLeftCircle
                 + LCourseSize.distanceBetweenCenterOfCirclesInBlockDeTreasure*2,
                 y: centerYOfBottomLeftCircle)
     }
-
+    
     // 赤の円のうち，左下のものの中心
     private var centerOfBottomLeftRed: CGPoint {
         CGPoint(x: centerXOfBottomLeftCircle,
                 y: centerYOfBottomLeftCircle
                 - LCourseSize.distanceBetweenCenterOfCirclesInBlockDeTreasure*2)
     }
-
+    
     // 黄色の円のうち，左下のものの中心
     private var centerOfBottomLeftYellow: CGPoint {
         CGPoint(x: centerXOfBottomLeftCircle
@@ -45,9 +45,48 @@ struct BlockDeTreasureView: View {
                 y: centerYOfBottomLeftCircle
                 - LCourseSize.distanceBetweenCenterOfCirclesInBlockDeTreasure*2)
     }
-
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // 青い円
+            BlockDeTreasureFourCirclesView(centerOfBottomLeftCircle: centerOfBottomLeftBlue,
+                                           color: .blue)
+            
+            // 緑の円
+            BlockDeTreasureFourCirclesView(centerOfBottomLeftCircle: centerOfBottomLeftGreen,
+                                           color: .green)
+            
+            // 赤の円
+            BlockDeTreasureFourCirclesView(centerOfBottomLeftCircle: centerOfBottomLeftRed,
+                                           color: .red)
+            
+            // 黄色の円
+            BlockDeTreasureFourCirclesView(centerOfBottomLeftCircle: centerOfBottomLeftYellow,
+                                           color: .yellow)
+            
+            // 水平の黒線
+            // 下から上
+            ForEach(0..<4) { index in
+                BlockDeTreasureHorizontalLinesView(
+                    leftEndPoint: CGPoint(x: centerXOfBottomLeftCircle
+                                          + LCourseSize.treasureCircleRadius
+                                          + LCourseSize.lineHalfWidth,
+                                          y: centerYOfBottomLeftCircle
+                                          - LCourseSize.distanceBetweenCenterOfCirclesInBlockDeTreasure*CGFloat(index)))
+            }
+            
+            // 垂直の黒線
+            // 左から右
+            ForEach(0..<4) { index in
+                BlockDeTreasureVerticalLinesView(
+                    bottomEndPoint: CGPoint(x: centerXOfBottomLeftCircle
+                                            + LCourseSize.distanceBetweenCenterOfCirclesInBlockDeTreasure*CGFloat(index),
+                                            y: centerYOfBottomLeftCircle
+                                            - LCourseSize.treasureCircleRadius
+                                            - LCourseSize.lineHalfWidth))
+            }
+        } // ZStack
+        .frame(width: LCourseViewSize.frameWidth, height: LCourseViewSize.frameHeight)
     } // var body
 } // struct BlockDeTreasureView
 
