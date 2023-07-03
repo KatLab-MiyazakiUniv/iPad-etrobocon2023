f4c6a4bbf86e00d6589cb76e8f73a1d30cf7d2ca
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:01:14 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/DoubleLoopExitBlackLineView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/DoubleLoopExitBlackLineView.swift
@@ -2,6 +2,45 @@ import SwiftUI
 
 /// ダブルループ後，コース下部の水平の直進と合流するカーブ
 struct DoubleLoopExitBlackLineView: View {
+    // start point
+    private var startX: CGFloat {
+        LCourseViewSize.startX - LCourseSize.blackLineCornerRadius - LCourseSize.bottomBlackStraightLineLength + LCourseSize.endOfCornerToBranchingLength
+    }
+
+    private var startY: CGFloat {
+        LCourseViewSize.startY + LCourseSize.startBlueLineLength + LCourseSize.rightBlackStraightLineLength + LCourseSize.blackLineCornerRadius
+    }
+
+    private var startPoint: CGPoint {
+        CGPoint(x: startX, y: startY)
+    }
+
+    // end point
+    private var endX: CGFloat {
+        LCourseSize.centerXOfA + LCourseSize.blackCircleRadiusOfA
+    }
+
+    private var endY: CGFloat {
+        LCourseSize.centerYOfA + 30
+    }
+
+    private var endPoint: CGPoint {
+        CGPoint(x: endX, y: endY)
+    }
+
+    // control point
+    private var controlX: CGFloat {
+        LCourseViewSize.startX - LCourseSize.blackLineCornerRadius - LCourseSize.bottomBlackStraightLineLength - LCourseSize.blackLineCornerRadius + 1200
+    }
+
+    private var controlY: CGFloat {
+        startY + 15
+    }
+
+    private var controlPoint: CGPoint {
+        CGPoint(x: controlX, y: controlY)
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
