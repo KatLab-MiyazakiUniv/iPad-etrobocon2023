4758d88fbc2e61c6c36e8eea87db3e9589692258
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:18:35 +0900

    add: BlockDeTreasureCircleView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureCircleView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+/// ライントレースの終端とブロックdeトレジャーの小さい円
+struct BlockDeTreasureCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct BlockDeTreasureCircleView
+
+struct BlockDeTreasureCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockDeTreasureCircleView()
+    }
+}
