008aca392105479cf2c872c9637a3a82a419d0e6
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:23:16 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/LineTraceEndCircleView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/LineTraceEndCircleView.swift
@@ -2,6 +2,19 @@ import SwiftUI
 
 /// ライントレース終端の赤い円
 struct LineTraceEndCircleView: View {
+    // center point
+    private var centerX: CGFloat {
+        LCourseViewSize.startX - LCourseSize.blackLineCornerRadius - LCourseSize.bottomBlackStraightLineLength - LCourseSize.blackLineCornerRadius
+    }
+
+    private var centerY: CGFloat {
+        LCourseViewSize.startY + LCourseSize.startBlueLineLength + LCourseSize.rightBlackStraightLineLength - LCourseSize.leftBlackStraightLineLength - LCourseSize.lineHalfWidth - LCourseSize.treasureCircleRadius
+    }
+
+    private var centerPoint: CGPoint {
+        CGPoint(x: centerX, y: centerY)
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
