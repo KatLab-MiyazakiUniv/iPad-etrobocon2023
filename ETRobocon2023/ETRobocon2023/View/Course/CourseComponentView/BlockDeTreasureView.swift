78d6422681deb30bb2aa8b95f54ef33b0f1f4b2c
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 16:19:57 +0900

    refactor: コードブロックの終端にコメントを追加した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureView.swift
@@ -4,8 +4,8 @@ import SwiftUI
 struct BlockDeTreasureView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    } // var body
+} // struct BlockDeTreasureView
 
 struct BlockDeTreasureView_Previews: PreviewProvider {
     static var previews: some View {
