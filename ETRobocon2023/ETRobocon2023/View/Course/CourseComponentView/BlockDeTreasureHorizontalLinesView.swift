70988308944cffc723e9d3b67e3adaccfbccf353
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:53:09 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureHorizontalLinesView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureHorizontalLinesView.swift
@@ -4,6 +4,47 @@ import SwiftUI
 struct BlockDeTreasureHorizontalLinesView: View {
     let leftEndPoint: CGPoint
 
+    // 左側の線分の右端
+    private var rightEndPointXOfLeftLine: CGFloat {
+        leftEndPoint.x + LCourseSize.blockDeTreasureBlackLineLength
+    }
+
+    // 中央の線分の左端
+    private var leftEndPointXOfCenterLine: CGFloat {
+        rightEndPointXOfLeftLine + LCourseSize.treasureCircleMaximumDiameter
+    }
+
+    private var leftEndPointOfCenterLine: CGPoint {
+        CGPoint(x: leftEndPointXOfCenterLine, y: leftEndPoint.y)
+    }
+
+    // 中央の線分の右端
+    private var rightEndPointXOfCenterLine: CGFloat {
+        leftEndPointXOfCenterLine + LCourseSize.treasureCircleMaximumDiameter
+    }
+
+    private var rightEndPointOfCenterLine: CGPoint {
+        CGPoint(x: rightEndPointXOfCenterLine, y: leftEndPoint.y)
+    }
+
+    // 右側の線分の左端
+    private var leftEndPointXOfRightLine: CGFloat {
+        rightEndPointXOfCenterLine + LCourseSize.treasureCircleMaximumDiameter
+    }
+
+    private var leftEndPointOfRightLine: CGPoint {
+        CGPoint(x: leftEndPointXOfRightLine, y: leftEndPoint.y)
+    }
+
+    // 右側の線分の右端
+    private var rightEndPointXOfRightLine: CGFloat {
+        leftEndPointXOfRightLine + LCourseSize.treasureCircleMaximumDiameter
+    }
+
+    private var rightEndPointOfRightLine: CGPoint {
+        CGPoint(x: rightEndPointXOfRightLine, y: leftEndPoint.y)
+    }
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
