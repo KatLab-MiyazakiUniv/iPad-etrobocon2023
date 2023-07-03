140a9e3d125392c9c70d79d4d84da39d5bc1327d
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 16:00:30 +0900

    add: BlockDeTreasureVerticalLinesView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureVerticalLinesView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// ブロックdeトレジャーの，垂直の黒線3本の組
+struct BlockDeTreasureVerticalLinesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct BlockDeTreasureVerticalLinesView
+
+struct BlockDeTreasureVerticalLinesView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlockDeTreasureVerticalLinesView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
