0292b0aaa931e10ead0b1fe1ebdf4c72b63c90ca
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:07:49 +0900

    add: DoubleLoopEntranceBlueStraightLineView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/DoubleLoopEntranceBlueStraightLineView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// コース左側の直進の青線
+struct DoubleLoopEntranceBlueStraightLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct DoubleLoopEntranceBlueStraightLineView
+
+struct DoubleLoopEntranceBlueStraightLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        DoubleLoopEntranceBlueStraightLineView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
