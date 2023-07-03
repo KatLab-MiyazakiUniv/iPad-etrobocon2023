830c664cc76b2caa70e3b579d13dce20f321aaf4
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 22:57:11 +0900

    add: SegmentNames列挙型を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/SegmentNames.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+enum SegmentNames {
+    case StartBlueLineView
+    case RightBlackStraightLineView
+    case RightBottomBlackCornerView
+    case BottomBlackStraightLineView
+    case LeftBottomBlackCornerView
+    case LeftBlackStraightLineView
+    case DoubleLoopEntranceBlackLineView
+    case AreaAView
+    case AreaBView
+    case DoubleLoopExitBlackLineView
+    case DoubleLoopEntranceBlueStraightLineView
+    case DoubleLoopEntranceBlueCurveLineView
+    case BlockDeTreasureCircleView
+    case LineTraceEndCircleView
+    case BlockDeTreasureView
+} // enum SegmentNames
