bb4b510d7b646f557f053cbfcb9f54d91eb57b42
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:41:49 +0900

    update: 引数を宣言し，プレビューを修正した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureHorizontalLinesView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureHorizontalLinesView.swift
@@ -2,14 +2,17 @@ import SwiftUI
 
 /// ブロックdeトレジャーの，水平の黒線3本の組
 struct BlockDeTreasureHorizontalLinesView: View {
+    let leftEndPoint: CGPoint
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
 } // struct BlockDeTreasureHorizontalLinesView
 
 struct BlockDeTreasureHorizontalLinesView_Previews: PreviewProvider {
+    static let leftMostPoint = CGPoint(x: 500, y: 500)
     static var previews: some View {
-        BlockDeTreasureHorizontalLinesView()
+        BlockDeTreasureHorizontalLinesView(leftEndPoint: leftMostPoint)
             .scaleEffect(LCourseViewSize.previewScale)
     }
 }
