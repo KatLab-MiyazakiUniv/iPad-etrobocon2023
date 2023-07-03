d0681f8b5319edda352e1f1f7e0eb1bba113ba58
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 11:54:27 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/StartBlueLineView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/StartBlueLineView.swift
@@ -1,6 +1,32 @@
 import SwiftUI
 
 struct StartBlueLineView: View {
+    // start point
+    private var startX: CGFloat {
+        LCourseViewSize.startX
+    }
+
+    private var startY: CGFloat {
+        LCourseViewSize.startY
+    }
+
+    private var startPoint: CGPoint {
+        CGPoint(x: startX, y: startY)
+    }
+
+    // end point
+    private var endX: CGFloat {
+        LCourseViewSize.startX
+    }
+
+    private var endY: CGFloat {
+        LCourseSize.startBlueLineLength
+    }
+
+    private var endPoint: CGPoint {
+        CGPoint(x: endX, y: endY)
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
