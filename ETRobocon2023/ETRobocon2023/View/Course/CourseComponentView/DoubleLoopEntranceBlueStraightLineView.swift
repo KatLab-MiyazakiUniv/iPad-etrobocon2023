91bc6099f6016535a683a444cfb88827b4ee528f
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:08:50 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/DoubleLoopEntranceBlueStraightLineView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/DoubleLoopEntranceBlueStraightLineView.swift
@@ -2,6 +2,32 @@ import SwiftUI
 
 /// コース左側の直進の青線
 struct DoubleLoopEntranceBlueStraightLineView: View {
+    // start point
+    private var startX: CGFloat {
+        LCourseViewSize.startX - LCourseSize.blackLineCornerRadius - LCourseSize.bottomBlackStraightLineLength - LCourseSize.blackLineCornerRadius
+    }
+
+    private var startY: CGFloat {
+        LCourseViewSize.startY + LCourseSize.startBlueLineLength + LCourseSize.rightBlackStraightLineLength - LCourseSize.endOfCornerToBranchingLength
+    }
+
+    private var startPoint: CGPoint {
+        CGPoint(x: startX, y: startY)
+    }
+
+    // end point
+    private var endX: CGFloat {
+        startX
+    }
+
+    private var endY: CGFloat {
+        startY - LCourseSize.doubleLoopEntranceBlueLineLength
+    }
+
+    private var endPoint: CGPoint {
+        CGPoint(x: endX, y: endY)
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
