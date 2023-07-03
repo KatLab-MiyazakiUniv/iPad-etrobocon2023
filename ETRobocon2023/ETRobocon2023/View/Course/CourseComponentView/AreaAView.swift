df9f1f7e83540528086a3accea2888999e9432bc
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 14:05:49 +0900

    add: AreaAView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/AreaAView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+/// 配置エリアA．
+/// 外側の黒い円，内側の黄色の円，中心の点線の円を描画する．
+struct AreaAView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AreaAView_Previews: PreviewProvider {
+    static var previews: some View {
+        AreaAView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
