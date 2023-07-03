20322dc5c107e39c91a81cc9fdc6fe55300366c4
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 22:36:46 +0900

    update: pathをプロパティとして切り出した
    ビルドが通り，シミュレータでは動作するが，プレビューではビルドに失敗する．

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/StartBlueLineView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/StartBlueLineView.swift
@@ -3,35 +3,63 @@ import SwiftUI
 /// スタート地点(より上部)の青線
 struct StartBlueLineView: View {
     // start point
-    private var startX: CGFloat {
-        LCourseViewSize.startX
-    }
+//    private var startX: CGFloat {
+//        LCourseViewSize.startX
+//    }
 
-    private var startY: CGFloat {
-        LCourseViewSize.startY
-    }
+//    private var startY: CGFloat {
+//        LCourseViewSize.startY
+//    }
 
-    private var startPoint: CGPoint {
-        CGPoint(x: startX, y: startY)
-    }
+//    private var startPoint: CGPoint {
+//        CGPoint(x: startX, y: startY)
+//    }
 
     // end point
-    private var endX: CGFloat {
-        LCourseViewSize.startX
-    }
+//    private var endX: CGFloat {
+//        LCourseViewSize.startX
+//    }
 
-    private var endY: CGFloat {
-        LCourseSize.startBlueLineLength
-    }
+//    private var endY: CGFloat {
+//        LCourseSize.startBlueLineLength
+//    }
 
-    private var endPoint: CGPoint {
-        CGPoint(x: endX, y: endY)
+//    private var endPoint: CGPoint {
+//        CGPoint(x: endX, y: endY)
+//    }
+
+    private let path: (inout Path) -> Void = { path in
+        var startX: CGFloat {
+            LCourseViewSize.startX
+        }
+
+        var startY: CGFloat {
+            LCourseViewSize.startY
+        }
+
+        var startPoint: CGPoint {
+            CGPoint(x: startX, y: startY)
+        }
+
+        var endX: CGFloat {
+            LCourseViewSize.startX
+        }
+
+        var endY: CGFloat {
+            LCourseSize.startBlueLineLength
+        }
+
+        var endPoint: CGPoint {
+            CGPoint(x: endX, y: endY)
+        }
+
+        path.move(to: startPoint)
+        path.addLine(to: endPoint)
     }
 
     var body: some View {
         Path { path in
-            path.move(to: startPoint)
-            path.addLine(to: endPoint)
+            self.path(&path)
         }
         .stroke(lineWidth: LCourseSize.lineWidth)
         .fill(.blue)
