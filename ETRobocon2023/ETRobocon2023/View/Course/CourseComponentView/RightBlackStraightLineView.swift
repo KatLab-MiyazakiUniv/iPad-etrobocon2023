ea2710f6c23a3f5664246f21ee8ac25d12f31d75
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 12:04:33 +0900

    update: Pathでコース右側の直進を描画した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/RightBlackStraightLineView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/RightBlackStraightLineView.swift
@@ -27,14 +27,21 @@ struct RightBlackStraightLineView: View {
     private var endPoint: CGPoint {
         CGPoint(x: endX, y: endY)
     }
-    
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Path { path in
+            path.move(to: startPoint)
+            path.addLine(to: endPoint)
+        }
+        .stroke(lineWidth: LCourseSize.lineWidth)
+        .fill(.black)
+        .frame(width: LCourseViewSize.frameWidth, height: LCourseViewSize.frameHeight)
     } // var body
 } // struct RightBlackStraightLineView
 
 struct RightBlackStraightLineView_Previews: PreviewProvider {
     static var previews: some View {
         RightBlackStraightLineView()
+            .scaleEffect(LCourseViewSize.previewScale)
     }
 }
