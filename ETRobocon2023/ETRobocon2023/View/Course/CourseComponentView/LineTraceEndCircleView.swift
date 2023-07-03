4e3b0e6b6f02ae8c5103c4c01aa40501353aab37
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:22:40 +0900

    add: LineTraceEndCircleView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/LineTraceEndCircleView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// ライントレース終端の赤い円
+struct LineTraceEndCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct LineTraceEndCircleView
+
+struct LineTraceEndCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        LineTraceEndCircleView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
