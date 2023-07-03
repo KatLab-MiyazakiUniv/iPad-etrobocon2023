f7c034e618b235e050ff7f05c5ffe541e6d2148d
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 13:59:38 +0900

    add: DoubleLoopEntranceBlackLineView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/DoubleLoopEntranceBlackLineView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+/// コース左側の直進からダブルループに枝分かれしたカーブ
+struct DoubleLoopEntranceBlackLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DoubleLoopEntranceBlackLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        DoubleLoopEntranceBlackLineView()
+            .scaleEffect(LCourseViewSize.previewScale)
+    }
+}
