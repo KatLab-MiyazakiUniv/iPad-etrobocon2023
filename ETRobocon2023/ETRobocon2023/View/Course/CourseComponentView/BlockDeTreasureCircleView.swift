9e292319a140ea2a439f3333754353ee0407d102
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:19:20 +0900

    update: 引数を宣言し，プレビューを修正した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureCircleView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureCircleView.swift
@@ -2,13 +2,18 @@ import SwiftUI
 
 /// ライントレースの終端とブロックdeトレジャーの小さい円
 struct BlockDeTreasureCircleView: View {
+    var center: CGPoint
+    var color: Color
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
 } // struct BlockDeTreasureCircleView
 
 struct BlockDeTreasureCircleView_Previews: PreviewProvider {
+    static let center = CGPoint(x: 500, y: 500)
+    static let color = Color.red
     static var previews: some View {
-        BlockDeTreasureCircleView()
+        BlockDeTreasureCircleView(center: center, color: color)
     }
 }
