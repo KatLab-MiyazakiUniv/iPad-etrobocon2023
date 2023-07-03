785839a79fba47599e8d5a095bef788701379ed7
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 16:11:54 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureVerticalLinesView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureVerticalLinesView.swift
@@ -4,6 +4,51 @@ import SwiftUI
 struct BlockDeTreasureVerticalLinesView: View {
     let bottomEndPoint: CGPoint
 
+    // 下側の線分の上端
+    private var topEndPointYOfBottomLine: CGFloat {
+        bottomEndPoint.y - LCourseSize.blockDeTreasureBlackLineLength
+    }
+
+    private var topEndPointOfBottomLine: CGPoint {
+        CGPoint(x: bottomEndPoint.x, y: topEndPointYOfBottomLine)
+    }
+
+    // 中央の線分の下端
+    private var bottomEndPointYOfCenterLine: CGFloat {
+        topEndPointYOfBottomLine - LCourseSize.treasureCircleMaximumDiameter
+    }
+
+    private var bottomEndPointOfCenterLine: CGPoint {
+        CGPoint(x: bottomEndPoint.x, y: bottomEndPointYOfCenterLine)
+    }
+
+    // 中央の線分の上端
+    private var topEndPointYOfCenterLine: CGFloat {
+        bottomEndPointYOfCenterLine - LCourseSize.blockDeTreasureBlackLineLength
+    }
+
+    private var topEndPointOfCenterLine: CGPoint {
+        CGPoint(x: bottomEndPoint.x, y: topEndPointYOfCenterLine)
+    }
+
+    // 上側の線分の下端
+    private var bottomEndPointYOfTopLine: CGFloat {
+        topEndPointYOfCenterLine - LCourseSize.treasureCircleMaximumDiameter
+    }
+
+    private var bottomEndPointOfTopLine: CGPoint {
+        CGPoint(x: bottomEndPoint.x, y: bottomEndPointYOfTopLine)
+    }
+
+    // 上側の線分の上端
+    private var topEndPointYOfTopLine: CGFloat {
+        bottomEndPointYOfTopLine - LCourseSize.blockDeTreasureBlackLineLength
+    }
+
+    private var topEndPointOfTopLine: CGPoint {
+        CGPoint(x: bottomEndPoint.x, y: topEndPointYOfTopLine)
+    }
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
