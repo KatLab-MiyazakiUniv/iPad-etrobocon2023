af6c67770b5fe92a6afe4064087dde62bad7421d
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 12:28:51 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/LeftBlackStraightLineView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/LeftBlackStraightLineView.swift
@@ -2,6 +2,32 @@ import SwiftUI
 
 /// コース左側の，スタート直後の直進
 struct LeftBlackStraightLineView: View {
+    // start point
+    private var startX: CGFloat {
+        LCourseViewSize.startX - LCourseSize.blackLineCornerRadius - LCourseSize.bottomBlackStraightLineLength - LCourseSize.blackLineCornerRadius
+    }
+
+    private var startY: CGFloat {
+        LCourseViewSize.startY + LCourseSize.startBlueLineLength + LCourseSize.rightBlackStraightLineLength
+    }
+
+    private var startPoint: CGPoint {
+        CGPoint(x: startX, y: startY)
+    }
+
+    // end point
+    private var endX: CGFloat {
+        startX
+    }
+
+    private var endY: CGFloat {
+        startY - LCourseSize.leftBlackStraightLineLength
+    }
+
+    private var endPoint: CGPoint {
+        CGPoint(x: endX, y: endY)
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
