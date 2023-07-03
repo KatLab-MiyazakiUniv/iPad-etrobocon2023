1c90a5e54346e6f7066c037b7feae89b9f93ed81
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 15:29:02 +0900

    update: 引数を宣言し，プレビューを修正した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureFourCirclesView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/BlockDeTreasureFourCirclesView.swift
@@ -2,14 +2,19 @@ import SwiftUI
 
 /// ブロックdeトレジャーの，同じ色の4つの円の組
 struct BlockDeTreasureFourCirclesView: View {
+    let centerOfBottomLeftCircle: CGPoint
+    let color: Color
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     } // var body
 } // struct BlockDeTreasureFourCirclesView
 
 struct BlockDeTreasureFourCirclesView_Previews: PreviewProvider {
+    static let center = CGPoint(x: 500, y: 500)
+    static let color = Color.blue
     static var previews: some View {
-        BlockDeTreasureFourCirclesView()
+        BlockDeTreasureFourCirclesView(centerOfBottomLeftCircle: center, color: color)
             .scaleEffect(LCourseViewSize.previewScale)
     }
 }
