b2a3faa02ce26acaffe8f50cbd5f0949b3a0592e
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 12:06:33 +0900

    add: RightBottomBlackCornerView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/RightBottomBlackCornerView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+/// コース右下の，黒線のカーブ
+struct RightBottomBlackCornerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct RightBottomBlackCornerView
+
+struct RightBottomBlackCornerView_Previews: PreviewProvider {
+    static var previews: some View {
+        RightBottomBlackCornerView()
+    }
+}
