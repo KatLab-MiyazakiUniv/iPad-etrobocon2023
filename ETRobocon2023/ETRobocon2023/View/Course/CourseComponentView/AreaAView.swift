0f6342d7beae66f80948af7cc4175f18825dfb11
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 14:06:32 +0900

    update: Pathでの描画に必要なCGPoint型プロパティを宣言した

--- ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/AreaAView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/AreaAView.swift
@@ -3,6 +3,9 @@ import SwiftUI
 /// 配置エリアA．
 /// 外側の黒い円，内側の黄色の円，中心の点線の円を描画する．
 struct AreaAView: View {
+
+    private var centerPoint = CGPoint(x: LCourseSize.centerXOfA, y: LCourseSize.centerYOfA)
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
