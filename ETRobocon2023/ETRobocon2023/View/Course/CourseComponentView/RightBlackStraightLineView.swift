621de74844768c05acea29915ad6b0dc8b145098
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 12:01:10 +0900

    add: RightBlackStraightLineView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/RightBlackStraightLineView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+/// コース右側の，スタート直後の直進
+struct RightBlackStraightLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct RightBlackStraightLineView
+
+struct RightBlackStraightLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        RightBlackStraightLineView()
+    }
+}
