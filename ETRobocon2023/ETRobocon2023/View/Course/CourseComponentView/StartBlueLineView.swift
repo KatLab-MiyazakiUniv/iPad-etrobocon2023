0f15619adef795b2879a557f59d533856dc2b4d2
OnagaHaruki <haloupgarou@gmail.com>
2023-07-03 11:53:02 +0900

    add: StartBlueLineView構造体を追加した

---
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseComponentView/StartBlueLineView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct StartBlueLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    } // var body
+} // struct StartBlueLineView
+
+struct StartBlueLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartBlueLineView()
+    }
+}
