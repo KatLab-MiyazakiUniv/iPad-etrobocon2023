85084215afebe125c282d17bcca8cf14d0051024
OnagaHaruki <haloupgarou@gmail.com>
2023-07-05 01:30:08 +0900

    refactor: コメントを追加し，改行を整えた

--- ETRobocon2023/ETRobocon2023/View/Course/CourseMapAndListView.swift
+++ ETRobocon2023/ETRobocon2023/View/Course/CourseMapAndListView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+/// コースマップと区間名のリストを横並びで表示するビュー
 struct CourseMapAndListView: View {
     @State private var isAnimating = true
     @State private var selection = SegmentNames.NONE
@@ -10,13 +11,17 @@ struct CourseMapAndListView: View {
                 CourseView(isAnimating: $isAnimating, segment: $selection)
                     .scaleEffect(geometry.size.height / LCourseViewSize.frameHeight * 0.9)
                     .frame(
-                        width: min(geometry.size.height * (LCourseViewSize.frameWidth / LCourseViewSize.frameHeight), geometry.size.width),
-                        height: min(geometry.size.height, geometry.size.width * (LCourseViewSize.frameHeight / LCourseViewSize.frameWidth))
+                        width: min(geometry.size.height
+                                   * (LCourseViewSize.frameWidth / LCourseViewSize.frameHeight),
+                                   geometry.size.width),
+                        height: min(geometry.size.height,
+                                    geometry.size.width
+                                    * (LCourseViewSize.frameHeight / LCourseViewSize.frameWidth))
                     )
-            }
+            } // GeometryReader
 
             SegmentNamesListView(selection: $selection)
-        }
+        } // HStack
     } // var body
 } // struct CourseMapAndListView
 
