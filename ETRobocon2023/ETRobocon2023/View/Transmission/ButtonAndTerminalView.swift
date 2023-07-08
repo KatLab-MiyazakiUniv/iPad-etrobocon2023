30abd90c45ea63149279ea0977a070c737c4eede
OnagaHaruki <haloupgarou@gmail.com>
2023-07-08 21:38:16 +0900

    update: サーバにサンプルファイルを送信する処理を呼び出すようにした

--- ETRobocon2023/ETRobocon2023/View/Transmission/ButtonAndTerminalView.swift
+++ ETRobocon2023/ETRobocon2023/View/Transmission/ButtonAndTerminalView.swift
@@ -1,19 +1,32 @@
 import SwiftUI
 
 struct ButtonAndTerminalView: View {
-    @ObservedObject var viewModel = TerminalViewModel()
+    @ObservedObject var terminalViewModel = TerminalViewModel()
+    let fileSendViewModel = FileSendViewModel()
 
     var body: some View {
         HStack {
             GeometryReader { geometry in
                 HStack {
-                    TerminalView(messages: $viewModel.messages, width: geometry.size.width/2)
+                    TerminalView(messages: $terminalViewModel.messages, width: geometry.size.width/2)
                         .frame(width: geometry.size.width/2, height: geometry.size.height)
                     
                     Button {
-                        viewModel.addMessage(generateRandomString(Int.random(in: 30...150)))
+//                        viewModel.addMessage(generateRandomString(Int.random(in: 30...150)))
+                        terminalViewModel.addMessage("Send sample CSV file to First machine.")
+                        terminalViewModel.addMessage(CsvFileManager.generateSampleFile())
+                        Task {
+                            let result = await fileSendViewModel.SendSampleFile()
+                            terminalViewModel.addMessage("\n" + result + "\n")
+                        }
                     } label: {
-                        Text("viewModel.addMessage(generateRandomString())")
+//                        Text("viewModel.addMessage(generateRandomString())")
+                        Text("Send sample CSV to First machine")
+                            .font(.system(size: 35))
+                            .foregroundColor(.white)
+                            .frame(width: 600, height: 100)
+                            .background(.cyan)
+                            .cornerRadius(20)
                     }
                     .frame(width: geometry.size.width/2)
                 }
@@ -34,5 +47,6 @@ struct ButtonAndTerminalView: View {
 struct ButtonAndTerminalView_Previews: PreviewProvider {
     static var previews: some View {
         ButtonAndTerminalView()
+            .previewLayout(.fixed(width: 2732, height: 2048))
     }
 }
