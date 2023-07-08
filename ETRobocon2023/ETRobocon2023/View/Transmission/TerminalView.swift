526aa978745e6626ff9e2a79b1f51025b464ff2f
OnagaHaruki <haloupgarou@gmail.com>
2023-07-08 19:28:30 +0900

    update: 行数が増えたとき自動で上にスクロールするようにした

--- ETRobocon2023/ETRobocon2023/View/Transmission/TerminalView.swift
+++ ETRobocon2023/ETRobocon2023/View/Transmission/TerminalView.swift
@@ -1,32 +1,36 @@
 import SwiftUI
 
 struct TerminalView: View {
-    @StateObject var viewModel = TerminalViewModel()
+    @Binding var messages: [String]
 
     var body: some View {
         ZStack {
             Color.black
                 .edgesIgnoringSafeArea(.all)
 
-            ScrollView {
-                ScrollViewReader { scrollView in
+            ScrollViewReader { scrollProxy in
+                ScrollView {
                     VStack(alignment: .leading, spacing: 0) {
-                        ForEach(viewModel.messages, id: \.self) { message in
+                        ForEach(messages, id: \.self) { message in
                             Text(message)
                                 .font(.system(size: 15, weight: .regular, design: .monospaced))
                                 .foregroundColor(.white)
                                 .padding([.top, .bottom], 2)
+                                .id(message)
                         } // ForEach
                     } // VStack
-                } // ScrollViewReader
-            } // ScrollView
-            .padding(.all, 10)
+                    .onChange(of: messages) { _ in
+                        scrollProxy.scrollTo(messages.last)
+                    }
+                } // ScrollView
+            } // ScrollViewReader
         } // ZStack
     } // var body
 } // TerminalView
 
 struct TerminalView_Previews: PreviewProvider {
+    @ObservedObject static var viewModel = TerminalViewModel()
     static var previews: some View {
-        TerminalView()
+        TerminalView(messages: $viewModel.messages)
     }
 }
