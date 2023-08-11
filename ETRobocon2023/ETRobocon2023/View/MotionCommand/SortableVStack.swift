3796c3f2be599afb3c21ad8afc9183478a8be14b
OnagaHaruki <haloupgarou@gmail.com>
2023-08-11 17:32:12 +0900

    update: 複数のセルをまたいだ並び替えをできるようにした

--- ETRobocon2023/ETRobocon2023/View/MotionCommand/SortableVStack.swift
+++ ETRobocon2023/ETRobocon2023/View/MotionCommand/SortableVStack.swift
@@ -2,62 +2,54 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 struct DraggableVStack: View {
-    @State private var items: [String] = ["Item 1", "Item 2", "Item 3"]
-    @State private var draggedItem: String?
+
+    @State var items = ["1", "2", "3"]
+    @State var draggedItem : String?
 
     var body: some View {
-        VStack {
-            ForEach(items, id: \.self) { item in
+        LazyVStack(spacing : 15) {
+            ForEach(items, id:\.self) { item in
                 HStack {
                     Text(item)
                     Spacer()
                     Image(systemName: "arrow.up.arrow.down.circle.fill")
                         .foregroundColor(.blue)
-                        .onDrag {
+                        .onDrag({
                             self.draggedItem = item
-                            return NSItemProvider(object: item as NSString)
-                        }
+                            return NSItemProvider(item: nil, typeIdentifier: item)
+                        })
                 }
                 .padding()
-                .background(Color.gray.opacity(0.2))
-                .cornerRadius(8)
-                .overlay(draggedItem == item ? Color.green.opacity(0.5) : Color.clear)
-                .onDrop(of: [UTType.plainText], delegate: MyDropDelegate(item: item, items: $items, draggedItem: $draggedItem))
+                .frame(minWidth:0, maxWidth:.infinity, minHeight:50)
+                .border(Color.black).background(Color.red)
+                .onDrop(of: [UTType.text], delegate: MyDropDelegate(item: item, items: $items, draggedItem: $draggedItem))
             }
         }
     }
 }
 
-struct MyDropDelegate: DropDelegate {
-    var item: String
-    @Binding var items: [String]
-    @Binding var draggedItem: String?
+struct MyDropDelegate : DropDelegate {
+
+    let item : String
+    @Binding var items : [String]
+    @Binding var draggedItem : String?
 
     func performDrop(info: DropInfo) -> Bool {
-        draggedItem = nil
         return true
     }
 
     func dropEntered(info: DropInfo) {
-        guard let fromItem = draggedItem else { return }
+        guard let draggedItem = self.draggedItem else {
+            return
+        }
 
-        let fromIndex = items.firstIndex(of: fromItem)!
-        let toIndex = items.firstIndex(of: item)!
-
-        if fromIndex != toIndex {
-            withAnimation {
-                items.remove(at: fromIndex)
-                items.insert(fromItem, at: toIndex)
+        if draggedItem != item {
+            let from = items.firstIndex(of: draggedItem)!
+            let to = items.firstIndex(of: item)!
+            withAnimation(.default) {
+                self.items.move(fromOffsets: IndexSet(integer: from), toOffset: to > from ? to + 1 : to)
             }
         }
-    }
-
-    func dropUpdated(info: DropInfo) -> DropProposal? {
-        return DropProposal(operation: .move)
-    }
-
-    func dropExited(info: DropInfo) {
-        draggedItem = nil
     }
 }
 
