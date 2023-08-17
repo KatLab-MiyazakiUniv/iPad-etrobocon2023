82f4d1ef1c10da2e5b591d32bc3c8a6ece3c0bf4
OnagaHaruki <haloupgarou@gmail.com>
2023-08-17 15:22:38 +0900

    update: ScrollViewからListに変更した

--- ETRobocon2023/ETRobocon2023/View/MotionCommand/SelectedCommandListView.swift
+++ ETRobocon2023/ETRobocon2023/View/MotionCommand/SelectedCommandListView.swift
@@ -6,27 +6,72 @@ struct SelectedCommandListView: View {
     @State private var sectionTopPositions: [SelectedCommandSectionEnum: CGFloat] = [:]
 
     var body: some View {
-        ScrollView(.vertical, showsIndicators: true) {
-            SelectedCommandSectionView(motionCommands: $viewModel.fromStartToDoubleLoopCommands,
-                                       sectionTitle: SelectedCommandSectionEnum.FromStartToDoubleLoop.description,
-                                       isSelected: viewModel.isSelectedSection == .FromStartToDoubleLoop)
-            .padding(.bottom, SelectedCommandViewInfo().padding*3)
+//        ScrollView(.vertical, showsIndicators: true) {
+//            SelectedCommandSectionView(motionCommands: $viewModel.fromStartToDoubleLoopCommands,
+//                                       sectionTitle: SelectedCommandSectionEnum.FromStartToDoubleLoop.description,
+//                                       isSelected: viewModel.isSelectedSection == .FromStartToDoubleLoop)
+//            .padding(.bottom, SelectedCommandViewInfo().padding*3)
+//
+//            SelectedCommandSectionView(motionCommands: $viewModel.fromAreaAToAreaBCommands,
+//                                       sectionTitle: SelectedCommandSectionEnum.FromAreaAToAreaB.description,
+//                                       isSelected: viewModel.isSelectedSection == .FromAreaAToAreaB)
+//            .padding(.bottom, SelectedCommandViewInfo().padding*3)
+//
+//            SelectedCommandSectionView(motionCommands: $viewModel.areaBCommands,
+//                                       sectionTitle: SelectedCommandSectionEnum.AreaB.description,
+//                                       isSelected: viewModel.isSelectedSection == .AreaB)
+//            .padding(.bottom, SelectedCommandViewInfo().padding*3)
+//            
+//            SelectedCommandSectionView(motionCommands: $viewModel.areaABottomCommands,
+//                                       sectionTitle: SelectedCommandSectionEnum.AreaABottom.description,
+//                                       isSelected: viewModel.isSelectedSection == .AreaABottom)
+//            .padding(.bottom, SelectedCommandViewInfo().padding*3)
+//        }
+//        .frame(width: SelectedCommandViewInfo().selectedCommandListWidth)
+//        .background(SelectedCommandViewInfo().selectedCommandListBackground)
 
-            SelectedCommandSectionView(motionCommands: $viewModel.fromAreaAToAreaBCommands,
-                                       sectionTitle: SelectedCommandSectionEnum.FromAreaAToAreaB.description,
-                                       isSelected: viewModel.isSelectedSection == .FromAreaAToAreaB)
-            .padding(.bottom, SelectedCommandViewInfo().padding*3)
+        List {
+            Section {
+                ForEach($viewModel.fromStartToDoubleLoopCommands) { $motionCommand in
+                    SelectedCommandCellView(motionCommand: $motionCommand)
+                }
+                .listRowSeparator(.hidden)
+            } header: {
+                Text(SelectedCommandSectionEnum.FromStartToDoubleLoop.description)
+                    .font(SelectedCommandViewInfo().commandTitleFont)
+            }
 
-            SelectedCommandSectionView(motionCommands: $viewModel.areaBCommands,
-                                       sectionTitle: SelectedCommandSectionEnum.AreaB.description,
-                                       isSelected: viewModel.isSelectedSection == .AreaB)
-            .padding(.bottom, SelectedCommandViewInfo().padding*3)
-            
-            SelectedCommandSectionView(motionCommands: $viewModel.areaABottomCommands,
-                                       sectionTitle: SelectedCommandSectionEnum.AreaABottom.description,
-                                       isSelected: viewModel.isSelectedSection == .AreaABottom)
-            .padding(.bottom, SelectedCommandViewInfo().padding*3)
+            Section {
+                ForEach($viewModel.fromAreaAToAreaBCommands) { $motionCommand in
+                    SelectedCommandCellView(motionCommand: $motionCommand)
+                }
+                .listRowSeparator(.hidden)
+            } header: {
+                Text(SelectedCommandSectionEnum.FromAreaAToAreaB.description)
+                    .font(SelectedCommandViewInfo().commandTitleFont)
+            }
+
+            Section {
+                ForEach($viewModel.areaBCommands) { $motionCommand in
+                    SelectedCommandCellView(motionCommand: $motionCommand)
+                }
+                .listRowSeparator(.hidden)
+            } header: {
+                Text(SelectedCommandSectionEnum.AreaB.description)
+                    .font(SelectedCommandViewInfo().commandTitleFont)
+            }
+
+            Section {
+                ForEach($viewModel.areaABottomCommands) { $motionCommand in
+                    SelectedCommandCellView(motionCommand: $motionCommand)
+                }
+                .listRowSeparator(.hidden)
+            } header: {
+                Text(SelectedCommandSectionEnum.AreaABottom.description)
+                    .font(SelectedCommandViewInfo().commandTitleFont)
+            }
         }
+        .listStyle(SidebarListStyle())
         .frame(width: SelectedCommandViewInfo().selectedCommandListWidth)
         .background(SelectedCommandViewInfo().selectedCommandListBackground)
     }
@@ -36,6 +81,7 @@ struct SelectedCommandListView_Previews: PreviewProvider {
     @State static var viewModel = SelectedCommandViewModel()
     static var previews: some View {
         SelectedCommandListView()
+            .environmentObject(SelectedCommandViewModel())
             .previewLayout(.fixed(width: 600, height: 1000))
     }
 }
