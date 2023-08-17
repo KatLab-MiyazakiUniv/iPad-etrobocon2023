470de16a274490c6794e20bdebf8bd50c2093831
OnagaHaruki <haloupgarou@gmail.com>
2023-08-17 20:45:26 +0900

    update: セルの境界線を削除した

--- ETRobocon2023/ETRobocon2023/View/MotionCommand/MotionCommandOptionList.swift
+++ ETRobocon2023/ETRobocon2023/View/MotionCommand/MotionCommandOptionList.swift
@@ -17,6 +17,7 @@ struct MotionCommandOptionList: View {
     var body: some View {
         List(MotionCommandEnum.allCases, id: \.self) { commandCase in
             MotionCommandOptionCell(motionCommandType: commandCase)
+                .listRowSeparator(.hidden)
         }
         .frame(width: 300)
     }
