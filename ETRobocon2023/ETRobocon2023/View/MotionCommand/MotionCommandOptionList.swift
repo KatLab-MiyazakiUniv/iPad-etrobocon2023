b1770c939e3d89a64fcd5de02e73f21566bd1541
OnagaHaruki <haloupgarou@gmail.com>
2023-08-17 20:46:36 +0900

    refactor: 不要なコードを削除した

--- ETRobocon2023/ETRobocon2023/View/MotionCommand/MotionCommandOptionList.swift
+++ ETRobocon2023/ETRobocon2023/View/MotionCommand/MotionCommandOptionList.swift
@@ -1,19 +1,6 @@
 import SwiftUI
 
 struct MotionCommandOptionList: View {
-    let motionCommands: [MotionCommand] = [MotionCommand(command: .DL, section: .FromStartToDoubleLoop),
-                                           MotionCommand(command: .CL, section: .FromStartToDoubleLoop),
-                                           MotionCommand(command: .DS, section: .FromStartToDoubleLoop),
-                                           MotionCommand(command: .CS, section: .FromStartToDoubleLoop),
-                                           MotionCommand(command: .AR, section: .FromStartToDoubleLoop),
-                                           MotionCommand(command: .DT, section: .FromStartToDoubleLoop),
-                                           MotionCommand(command: .EC, section: .FromStartToDoubleLoop),
-                                           MotionCommand(command: .SL, section: .FromStartToDoubleLoop),
-//                                           MotionCommand(command: .AU),
-//                                           MotionCommand(command: .AD),
-                                           MotionCommand(command: .XR, section: .FromStartToDoubleLoop)
-    ]
-
     var body: some View {
         List(MotionCommandEnum.allCases, id: \.self) { commandCase in
             MotionCommandOptionCell(motionCommandType: commandCase)
